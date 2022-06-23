27dc2b482671c1394f4f06a12a2f669cbdcf08d8
TXINX <tangxinrt@gmail.com>
2022-06-24 04:44:24 +0800

    Commit all.

---
+++ Source/TicTacToe/UIMessageManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UIMessageManager.h"
+
+UIMessageManager::UIMessageManager()
+{
+}
+
+UIMessageManager::~UIMessageManager()
+{
+}
